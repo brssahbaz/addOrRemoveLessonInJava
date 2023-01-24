2d55e03a8ed53f712dfa1cd8b4cad119b8c0719f
Brssahbaz <shbzbrs@gmail.com>
2023-01-24 20:25:00 +0300

    AddOrremoveLesson

---
+++ Runner.java
@@ -0,0 +1,24 @@
+package Clarus_JavaPracticeFall.StudentCodeChallenge.Yeni;
+
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+Student student = new Student();
+student.ogrenciKaydet();
+
+
+    }// main sonu
+
+
+
+
+
+
+
+}
